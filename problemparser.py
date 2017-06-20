04b11df17a6da495cf6cd73595f4239c48c1a681
Renan Tashiro <renantashiro@hotmail.com>
2017-06-20 13:26:57 -0300

    prototipo feito

--- problemparser.py
+++ problemparser.py
@@ -7,20 +7,20 @@ import threading
 from bs4 import BeautifulSoup
 
 
-# conta quantas páginas de problemas resolvidos um perfil possui
+# conta quantas páginas de problemas resolvidos um perfil possui.
 def n_of_pages(page):
     content = page.div.find(id='table-info').contents[0]  # i.e "1 of 32"
     return [int(i) for i in content.split() if i.isdigit()][-1]
 
 
-# extrai o id do problema e o seu url
+# extrai o id do problema e o seu url.
 def problem_row_data(tr):
     for td in tr.find_all('td'):
         if td.a:
             return td.a.contents[0], td.a.get('href')
 
 
-# extrai quando um problema foi resolvido
+# extrai quando um problema foi resolvido.
 def solved_date(tr):
     for td in tr.find_all('td'):
         if not td.a:
@@ -29,7 +29,7 @@ def solved_date(tr):
                 return s.group(0)
 
 
-# extrai a categoria de um problema e seu level
+# extrai a categoria de um problema e seu level.
 def find_problem_info(url):
     problem_url = 'https://www.urionlinejudge.com.br' + url
     page = BeautifulSoup(urlopen(problem_url), 'html.parser')
@@ -38,7 +38,7 @@ def find_problem_info(url):
     return category, int(level)
 
 
-# extrai informações dos problemas resolvidos de uma página
+# extrai informações dos problemas resolvidos de uma página.
 def scraper(url, data):
     page = BeautifulSoup(urlopen(url), 'html.parser')
     for tr in page.tbody.find_all('tr'):
@@ -49,10 +49,10 @@ def scraper(url, data):
         data.put((date, problem_id, category, level))
 
 
-# TODO: colocar um try...except quando ele faz um urlopen.
-def main():
-    profile_url = 'https://www.urionlinejudge.com.br/judge/pt/profile/38388'
-    page_url_base = 'https://www.urionlinejudge.com.br/judge/pt/profile/38388?page='
+# TODO: colocar um try...except quando ele faz um urlopen; colocar label no csv, para torna-lo mais flexivel
+def main(profile_id):
+    profile_url = 'https://www.urionlinejudge.com.br/judge/pt/profile/%s' % profile_id
+    page_url_base = 'https://www.urionlinejudge.com.br/judge/pt/profile/%s?page=' % profile_id
     soup = BeautifulSoup(urlopen(profile_url), 'html.parser')
     threads = []
     sync_data = queue.Queue()
@@ -65,27 +65,28 @@ def main():
         thread.start()
         threads.append(thread)
 
-    # espera todas as threads terminarem suas tarefas
+    # espera todas as threads terminarem suas tarefas.
     for thread in threads:
         thread.join()
 
     data = []
 
-    # extrai as informações da fila
+    # extrai as informações da fila.
     while not sync_data.empty():
         data.append(sync_data.get())
 
-    # extrai o id do perfil, com base na url de entrada
+    # extrai o id do perfil, com base na url de entrada.
     profile_id = profile_url.rsplit('/', 1)[-1]
 
-    # ordena pela data que o problema foi resolvido
+    # ordena pela data que o problema foi resolvido.
     sdata = sorted(data, key=lambda d: list(map(int, d[0].split('/')[::-1])))
 
-    # salvas as informações de cada problema em uma arquivo csv
+    # salvas as informações de cada problema em uma arquivo csv.
     with open(profile_id + '.csv', 'w+') as csvfile:
         writer = csv.writer(csvfile)
         for row in sdata:
             writer.writerow(row)
 
+
 if __name__ == '__main__':
-    main()
+    main(38388)
--- statistics.py
+++ statistics.py
@@ -5,23 +5,27 @@ import math
 import matplotlib.pyplot as plt
 
 
+# carrega os dados de algum perfil da saída gerada pelo problemparser.
 def load_data(profile_id):
     with open(profile_id + '.csv') as csvfile:
         reader = csv.reader(csvfile)
         return list(reader)
 
 
+# calcula a diferença, em dias, de duas datas.
 def date_distance(d, d0):
     dd0, mm0, yy0 = map(int, d0.split('/'))
     dd, mm, yy = map(int, d.split('/'))
     return (date(yy, mm, dd) - date(yy0, mm0, dd0)).days
 
 
-def score(data, range=None):
+# calcula a pontuação de algum perfil, retornando uma lista de tuplas
+# com o dia e a pontuação associada a esse dia.
+def score(data, last_day=None):
     value = 0
     actual_date = data[0][0]
     for problem in data:
-        if range and date_distance(problem[0], data[0][0]) > range:
+        if last_day and date_distance(problem[0], data[0][0]) > last_day:
             break
         if actual_date != problem[0]:
             actual_date = problem[0]
@@ -29,20 +33,68 @@ def score(data, range=None):
         value += int(problem[-1]) ** math.e
 
 
-def score_over_time(profiles_ids):
-    for id in profiles_ids:
-        data = load_data(id)
+# calcula quantos problemas foram resolvidos em uma determinada categoria,
+# retornando uma lista d e tuplas com o dia e a quantidade de cada categoria.
+def category_solved(profile_id, last_day=None):
+    category = {
+        'Iniciante': 0, 'Ad-Hoc': 0,
+        'Strings': 0, 'Estruturas e Bibliotecas': 0,
+        'Matemática': 0, 'Paradigmas': 0,
+        'Grafos': 0, 'Geometria Computacional': 0
+    }
+    data = load_data(profile_id)
+    actual_date = data[0][0]
+    for problem in data:
+        if last_day and date_distance(problem[0], data[0][0]) > last_day:
+            break
+        if actual_date != problem[0]:
+            actual_date = problem[0]
+            yield date_distance(actual_date, data[0][0]), \
+                  category['Iniciante'], category['Ad-Hoc'], \
+                  category['Strings'], category['Estruturas e Bibliotecas'], \
+                  category['Matemática'], category['Paradigmas'], \
+                  category['Grafos'], category['Geometria Computacional']
+        category[problem[2]] += 1
+
+
+# mostra uma gráfico que indica a pontuação de um perfil, desde
+# que sua conta foi criada.
+def show_score_over_time(profiles_ids):
+    for profile_id in profiles_ids:
+        data = load_data(profile_id)
         x, y = zip(*score(data))
-        plt.plot(x, y, label=id)
-    plt.title('score')
-    plt.xlabel('day')
-    plt.ylabel('score')
+        plt.plot(x, y, label=profile_id)
+    plt.title('Score')
+    plt.xlabel('Day')
+    plt.ylabel('Score')
+    plt.legend()
+    plt.show()
+
+
+# mostra um gráfico que indica quantos problemas foram resolvidos
+# em uma determinada categoria, desde que sua conta foi criada.
+def show_problem_solved_by_category(profiles_ids, categories=None):
+    categories_names = [
+        'Iniciante', 'Ad-Hoc', 'Strings', 'Estruturas e Bibliotecas',
+        'Matemática', 'Paradigmas', 'Grafos', 'Geometrica Computacional'
+    ]
+    if not categories:
+        categories = categories_names
+    for profile_id in profiles_ids:
+        x, *category = zip(*category_solved(profile_id))
+        for label, y in zip(categories_names, category):
+            if label in categories:
+                plt.plot(x, y, label='%s (%s)' % (label, profile_id))
+    plt.title('Number of Problem Solved')
+    plt.xlabel('Day')
+    plt.ylabel('Problems Solved')
     plt.legend()
     plt.show()
 
 
 def main():
-    score_over_time(['20268', '36720', '38388', '171690', '171692'])  # Luis, Renan, Marcelo, Gabriel, Paola
+    show_score_over_time(['20268', '36720', '38388', '171690', '171692'])  # Luis, Renan, Marcelo, Gabriel, Paola
+    show_problem_solved_by_category(['171692', '36720'], ['Iniciante', 'Grafos', 'Paradigmas'])  # Luis, Renan
 
 
 if __name__ == '__main__':
